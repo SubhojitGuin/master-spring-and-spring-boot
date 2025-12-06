a57e351e12f83f0cbd6af2d57cee5661f896b542
Subhojit Guin <subhojitguin2004@gmail.com>
2025-12-06 20:31:10 +0530

    Move the configuration and Beans in the main class

--- learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/App03GamingSpringBeans.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/App03GamingSpringBeans.java
@@ -4,11 +4,24 @@ import com.in28minutes.learn_spring_framework.game.GameRunner;
 import com.in28minutes.learn_spring_framework.game.GamingConsole;
 import com.in28minutes.learn_spring_framework.game.PacManGame;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class App03GamingSpringBeans {
+    @Bean
+    public GamingConsole game() {
+        return new PacManGame();
+    }
+
+    @Bean
+    public GameRunner gameRunner(GamingConsole game) {
+        return new GameRunner(game);
+    }
+
     public static void main(String[] args) {
         try (AnnotationConfigApplicationContext context =
-                     new AnnotationConfigApplicationContext(GamingConfiguration.class)) {
+                     new AnnotationConfigApplicationContext(App03GamingSpringBeans.class)) {
             context.getBean(GamingConsole.class).up();
             context.getBean(GameRunner.class).run();
         }
