29211503f325a63ecd4004680941d9caa1837a51
Subhojit Guin <subhojitguin2004@gmail.com>
2025-12-06 20:26:37 +0530

    Create a copy: learn-spring-framework-02

---
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/App03GamingSpringBeans.java
@@ -0,0 +1,16 @@
+package com.in28minutes.learn_spring_framework;
+
+import com.in28minutes.learn_spring_framework.game.GameRunner;
+import com.in28minutes.learn_spring_framework.game.GamingConsole;
+import com.in28minutes.learn_spring_framework.game.PacManGame;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App03GamingSpringBeans {
+    public static void main(String[] args) {
+        try (AnnotationConfigApplicationContext context =
+                     new AnnotationConfigApplicationContext(GamingConfiguration.class)) {
+            context.getBean(GamingConsole.class).up();
+            context.getBean(GameRunner.class).run();
+        }
+    }
+}
