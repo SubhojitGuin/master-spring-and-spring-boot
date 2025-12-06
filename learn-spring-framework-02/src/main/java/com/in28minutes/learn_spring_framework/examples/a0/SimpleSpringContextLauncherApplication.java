acdc7dc202e89bef037f5d78f05109cf5f3a60b2
Subhojit Guin <subhojitguin2004@gmail.com>
2025-12-06 22:22:18 +0530

    Rename GamingAppLauncherApplication and add SimpleSpringContextLauncherApplication to examples.a0

---
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/examples/a0/SimpleSpringContextLauncherApplication.java
@@ -0,0 +1,18 @@
+package com.in28minutes.learn_spring_framework.examples.a0;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+@ComponentScan // Uses the current folder as the default path
+public class SimpleSpringContextLauncherApplication {
+    public static void main(String[] args) {
+        try (AnnotationConfigApplicationContext context =
+                     new AnnotationConfigApplicationContext(SimpleSpringContextLauncherApplication.class)) {
+            Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+        }
+    }
+}
