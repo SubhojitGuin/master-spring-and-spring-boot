91424343229fadf97a30293f055820a0c89a1b07
Subhojit Guin <subhojitguin2004@gmail.com>
2025-12-09 16:57:58 +0530

    Implement CourseSpringDataJpaRepository and update CourseCommandLineRunner to use Spring Data JPA methods

--- 03-jpa-getting-started/learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learn_jpa_and_hibernate/course/CourseCommandLineRunner.java
+++ 03-jpa-getting-started/learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learn_jpa_and_hibernate/course/CourseCommandLineRunner.java
@@ -2,6 +2,7 @@ package com.in28minutes.springboot.learn_jpa_and_hibernate.course;
 
 import com.in28minutes.springboot.learn_jpa_and_hibernate.course.jdbc.CourseJdbcRepository;
 import com.in28minutes.springboot.learn_jpa_and_hibernate.course.jpa.CourseJpaRepository;
+import com.in28minutes.springboot.learn_jpa_and_hibernate.course.springdatajpa.CourseSpringDataJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,18 +13,24 @@ public class CourseCommandLineRunner implements CommandLineRunner {
 //    @Autowired
 //    private CourseJdbcRepository repository;
 
+//    @Autowired
+//    private CourseJpaRepository repository;
+
     @Autowired
-    private CourseJpaRepository repository;
+    private CourseSpringDataJpaRepository repository;
 
     @Override
     public void run(String ... args) throws Exception {
-        repository.insert(new Course(1, "Learn AWS!", "Subho"));
-        repository.insert(new Course(2, "Learn Azure!", "Subho"));
-        repository.insert(new Course(3, "Learn DevOps!", "Subho"));
+        repository.save(new Course(1, "Learn AWS!", "Subho"));
+        repository.save(new Course(2, "Learn Azure!", "Subho"));
+        repository.save(new Course(3, "Learn DevOps!", "Subho"));
 
-        repository.deleteById(1);
+        repository.deleteById(1L);
 
-        System.out.println(repository.findById(2));
-        System.out.println(repository.findById(3));
+        System.out.println(repository.findById(2L));
+        System.out.println(repository.findById(3L));
+
+        System.out.println(repository.findAll());
+        System.out.println(repository.count());
     }
 }
