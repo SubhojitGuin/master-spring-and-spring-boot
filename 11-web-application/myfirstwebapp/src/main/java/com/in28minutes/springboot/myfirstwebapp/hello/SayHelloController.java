377b870537d5f9bfe9f4346c4760533442ad58d8
Subhojit Guin <subhojitguin2004@gmail.com>
2025-12-10 12:11:45 +0530

    Add SayHelloController with /say-hello and /say-hello-html endpoints, and update application properties with port and pom.xml for new dependencies

---
+++ 11-web-application/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -0,0 +1,30 @@
+package com.in28minutes.springboot.myfirstwebapp.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller //To tell Spring that it handles web requests
+public class SayHelloController {
+    @RequestMapping("say-hello")
+    @ResponseBody // Returns the message as it is to the browser
+    public String sayHello() {
+        return "Hello! What are you learning today?";
+    }
+
+    @RequestMapping("say-hello-html")
+    @ResponseBody
+    public String sayHelloHtml() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("<html>");
+        sb.append("<head>");
+        sb.append("<title> My First HTML Page </title>");
+        sb.append("</head>");
+        sb.append("<body>");
+        sb.append("<h1> My First HTML Page with body </h1>");
+        sb.append("</body>");
+        sb.append("</html>");
+
+        return sb.toString();
+    }
+}
