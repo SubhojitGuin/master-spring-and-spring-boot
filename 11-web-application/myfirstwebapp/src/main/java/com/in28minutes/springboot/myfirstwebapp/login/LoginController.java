7e5fab2f1156daf159f2a096e239cb50579a9876
Subhojit Guin <subhojitguin2004@gmail.com>
2025-12-10 15:26:41 +0530

    Add LoginController and JSP view for login page

--- 11-web-application/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ 11-web-application/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -1,13 +1,18 @@
 package com.in28minutes.springboot.myfirstwebapp.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
     @RequestMapping("login")
-    public String login() {
+    public String login(@RequestParam String name, ModelMap model) {
+        // @RequestParam takes the name as a query parameter like ?name=Subho and the ModelMap stores the values to display in the JSP file as ${var_name}
+        model.put("name", name);
+        System.out.println("Request param is " + name);
         return "login";
     }
 }
--- 11-web-application/myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+++ 11-web-application/myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -3,6 +3,6 @@
         <title>Login Page</title>
     </head>
     <body>
-        <p>Welcome to the login page!</p>
+        <p>Welcome to the login page ${name}!</p>
     </body>
 </html>
