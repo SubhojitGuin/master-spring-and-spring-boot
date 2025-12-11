461a8f3c15db75f33ea53b677446b769ac20af6f
Subhojit Guin <subhojitguin2004@gmail.com>
2025-12-11 11:13:40 +0530

    Add Todo model, service, and controller; create listTodos.jsp for displaying todos

---
+++ 11-web-application/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
@@ -0,0 +1,24 @@
+package com.in28minutes.springboot.myfirstwebapp.todo;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class TodoService {
+    private static List<Todo> todos = new ArrayList<>();
+    static {
+        todos.add(new Todo(1, "Subho", "Learn AWS",
+                LocalDate.now().plusYears(1), false));
+        todos.add(new Todo(1, "Subho", "Learn Azure",
+                LocalDate.now().plusYears(2), false));
+        todos.add(new Todo(1, "Subho", "Learn GCP",
+                LocalDate.now().plusYears(3), false));
+    }
+
+    public List<Todo> findByUsername(String username) {
+        return todos;
+    }
+}
---
+++ 11-web-application/myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/listTodos.jsp
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Todos Page</title>
+    </head>
+    <body>
+        <div>
+            <p>Welcome to the Todos page!</p>
+            <div>Your todos are: ${todos}</div>
+        </div>
+    </body>
+</html>
