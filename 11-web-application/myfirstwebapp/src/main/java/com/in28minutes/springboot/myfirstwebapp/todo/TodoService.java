60c976503633c94bc363612f5a00eb1d75d575d7
Subhojit Guin <subhojitguin2004@gmail.com>
2025-12-11 16:05:06 +0530

    Add functionality to create new todos; implement TodoController methods for displaying and adding todos; create todo.jsp for todo entry form; update listTodos.jsp with link to add new todo

--- 11-web-application/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
+++ 11-web-application/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
@@ -9,16 +9,23 @@ import java.util.List;
 @Service
 public class TodoService {
     private static List<Todo> todos = new ArrayList<>();
+    private static int todosCount = 0;
+
     static {
-        todos.add(new Todo(1, "Subho", "Learn AWS",
+        todos.add(new Todo(++todosCount, "Subho", "Learn AWS",
                 LocalDate.now().plusYears(1), false));
-        todos.add(new Todo(2, "Subho", "Learn Azure",
+        todos.add(new Todo(++todosCount, "Subho", "Learn Azure",
                 LocalDate.now().plusYears(2), false));
-        todos.add(new Todo(3, "Subho", "Learn GCP",
+        todos.add(new Todo(++todosCount, "Subho", "Learn GCP",
                 LocalDate.now().plusYears(3), false));
     }
 
     public List<Todo> findByUsername(String username) {
         return todos;
+    }
+
+    public void addTodo(String username, String description, LocalDate targetDate, boolean done) {
+        Todo todo = new Todo(++todosCount, username, description, targetDate, done);
+        todos.add(todo);
     }
 }
