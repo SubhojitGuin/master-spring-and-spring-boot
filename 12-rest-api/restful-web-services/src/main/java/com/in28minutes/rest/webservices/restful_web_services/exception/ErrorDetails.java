7c2457e7ee16ab595c880115e487fade51f4140d
Subhojit Guin <subhojitguin2004@gmail.com>
2025-12-13 21:29:34 +0530

    Add CustomizedResponseEntityExceptionHandler and ErrorDetails for global exception handling

---
+++ 12-rest-api/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/exception/ErrorDetails.java
@@ -0,0 +1,40 @@
+package com.in28minutes.rest.webservices.restful_web_services.exception;
+
+import java.time.LocalDateTime;
+
+public class ErrorDetails {
+
+    private LocalDateTime timestamp;
+    private String message;
+    private String description;
+
+    public ErrorDetails(LocalDateTime timestamp, String message, String description) {
+        this.timestamp = timestamp;
+        this.message = message;
+        this.description = description;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
