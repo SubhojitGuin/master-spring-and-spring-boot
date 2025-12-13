83bfb4b65a180c9e1b2a2e9b8c8c53fbd386ccfc
Subhojit Guin <subhojitguin2004@gmail.com>
2025-12-13 20:59:34 +0530

    Add User model, UserDaoService, and UserResource for user management and UserNotFoundException

---
+++ 12-rest-api/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
@@ -0,0 +1,45 @@
+package com.in28minutes.rest.webservices.restful_web_services.user;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class UserResource {
+
+    public UserResource(UserDaoService service) {
+        this.service = service;
+    }
+
+    private UserDaoService service;
+
+    @GetMapping(value = "/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id) {
+        User user = service.getOne(id);
+
+        if (user == null)
+            throw new UserNotFoundException("id:"+id);
+
+        return user;
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest() // Current URI /users
+                .path("/{id}") // Adds id to the end of the URI /users/{id}
+                .buildAndExpand(savedUser.getId()) // Assigns value to id, like 4
+                .toUri(); // Converts it into URI format;
+
+        return ResponseEntity.created(location).build(); // ResponseEntity.created provides the status code of 201 - created.
+    }
+}
