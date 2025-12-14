bbd61b562677246312675e996422a12c940407c6
Subhojit Guin <subhojitguin2004@gmail.com>
2025-12-14 20:09:26 +0530

    Add JSON property annotations to User class for custom serialization of name and birthDate fields

--- 12-rest-api/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/User.java
+++ 12-rest-api/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/User.java
@@ -1,5 +1,6 @@
 package com.in28minutes.rest.webservices.restful_web_services.user;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -9,9 +10,11 @@ public class User {
     private Integer id;
 
     @Size(min = 2, message = "Name should contain atleast 2 characters")
+    @JsonProperty("user_name") // Used to change the name of the variable in the JSON response
     private String name;
 
     @Past(message = "Birth date should be in the past")
+    @JsonProperty("birth_date") // Change the key name from birthDate to birth_date in the JSON response
     private LocalDate birthDate;
 
     public User(Integer id, String name, LocalDate birthDate) {
