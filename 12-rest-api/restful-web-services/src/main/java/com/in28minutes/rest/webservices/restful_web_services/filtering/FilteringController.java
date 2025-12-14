703b476e762f1a3cc3c4f2d6930fe2ee73424bbe
Subhojit Guin <subhojitguin2004@gmail.com>
2025-12-14 21:07:41 +0530

    Add dynamic JSON filtering to FilteringController and SomeBean with JsonView support

--- 12-rest-api/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/filtering/FilteringController.java
+++ 12-rest-api/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/filtering/FilteringController.java
@@ -1,7 +1,12 @@
 package com.in28minutes.rest.webservices.restful_web_services.filtering;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tools.jackson.databind.json.JsonMapper;
+import tools.jackson.databind.ser.FilterProvider;
+import tools.jackson.databind.ser.std.SimpleBeanPropertyFilter;
+import tools.jackson.databind.ser.std.SimpleFilterProvider;
 
 import java.util.Arrays;
 import java.util.List;
@@ -10,13 +15,26 @@ import java.util.List;
 public class FilteringController {
 
     @GetMapping("/filtering")
-    public SomeBean filtering() {
-        return new SomeBean("value1", "value2", "value3");
+    @JsonView(Views.Public.class)
+    public String filtering() {
+        SomeBean someBean = new SomeBean("value1", "value2", "value3");
+
+        // Dynamic Filtering - Used to filter some fields for a particular API by passing instructions during parsing by Jackson
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1", "field3");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+        JsonMapper jsonMapper = JsonMapper.builder().build();
+
+        return jsonMapper.writer(filters).writeValueAsString(someBean);
     }
 
    @GetMapping("/filtering-list")
-    public List<SomeBean> filteringList() {
-        return Arrays.asList(new SomeBean("value1", "value2", "value3"),
-                new SomeBean("value4", "value5", "value6"));
+    public String filteringList() {
+        List<SomeBean> list = Arrays.asList(new SomeBean("value1", "value2", "value3"), new SomeBean("value4", "value5", "value6"));
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field2", "field3");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+        JsonMapper jsonMapper = JsonMapper.builder().build();
+
+        return jsonMapper.writer(filters).writeValueAsString(list);
     }
 }
--- 12-rest-api/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/filtering/SomeBean.java
+++ 12-rest-api/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/filtering/SomeBean.java
@@ -1,17 +1,23 @@
 package com.in28minutes.rest.webservices.restful_web_services.filtering;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonView;
 
 //@JsonIgnoreProperties({"field1", "field2"}) // For ignoring multiple fields together
-@JsonIgnoreProperties("field1") // Static Filtering at the class level - Ignoring all the fields in all JSON responses that are defined in the property
+//@JsonIgnoreProperties("field1") // Static Filtering at the class level - Ignoring all the fields in all JSON responses that are defined in the property
+@JsonFilter("SomeBeanFilter") // Dynamic Filtering - Map to the filter in FilterController
 public class SomeBean {
+    @JsonView(Views.Public.class)
     private String field1;
 
     // More Preferred
-    @JsonIgnore // Static Filtering - Removes this field from all JSON responses which contain this Bean
+//    @JsonIgnore // Static Filtering - Removes this field from all JSON responses which contain this Bean
+    @JsonView(Views.Public.class)
     private String field2;
 
+    @JsonView(Views.Public.class)
     private String field3;
 
     public SomeBean(String field1, String field2, String field3) {
