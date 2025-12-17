80c0bda0f4226404b7d342b73bcb7a0709985b3e
Subhojit Guin <subhojitguin2004@gmail.com>
2025-12-17 13:23:15 +0530

    Create the BusinessService1 and the DataService1; Run the BusinessService1 in LearnSpringAopApplication using CommandLineRunner

--- 73-spring-aop/learn-spring-aop/src/main/java/com/in28minutes/learn_spring_aop/LearnSpringAopApplication.java
+++ 73-spring-aop/learn-spring-aop/src/main/java/com/in28minutes/learn_spring_aop/LearnSpringAopApplication.java
@@ -1,13 +1,30 @@
 package com.in28minutes.learn_spring_aop;
 
+import com.in28minutes.learn_spring_aop.aopexample.business.BusinessService1;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// CommandLineRunner - to execute logic at the start of the application
 @SpringBootApplication
-public class LearnSpringAopApplication {
+public class LearnSpringAopApplication implements CommandLineRunner {
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	private BusinessService1 businessService1;
+
+	public LearnSpringAopApplication(BusinessService1 businessService1) {
+		this.businessService1 = businessService1;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearnSpringAopApplication.class, args);
 	}
 
+	// Runs the logic(business logic) at application startup
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("Value returned is {}", businessService1.calculateMax());
+	}
 }
