bb1ca354a2a8097153dc3b5eb45fe0476ef4274f
Subhojit Guin <subhojitguin2004@gmail.com>
2025-12-21 17:53:47 +0530

    Refactor App component to import and utilize individual components for better structure and readability

--- 13-full-stack/01-frontend/todo-app/src/App.js
+++ 13-full-stack/01-frontend/todo-app/src/App.js
@@ -1,4 +1,8 @@
-import { Component } from 'react';
+import FirstComponent from './components/learning-examples/FirstComponent'; // to import the default one, no {} are required. So, when imported without {}, it refers to the default thing
+import SecondComponent from './components/learning-examples/SecondComponent';
+import ThirdComponent from './components/learning-examples/ThirdComponent';
+import FourthComponent from './components/learning-examples/FourthComponent';
+import { FifthComponent } from './components/learning-examples/FirstComponent'; // to import a specific thing enclose within {}
 import './App.css';
 
 function App() {
@@ -9,36 +13,9 @@ function App() {
       <SecondComponent></SecondComponent>
       <ThirdComponent></ThirdComponent>
       <FourthComponent></FourthComponent>
+      <FifthComponent></FifthComponent>
     </div>
   );
-}
-
-function FirstComponent() {
-  return (
-    <div className="FirstComponent">First Component</div>
-  );
-}
-
-function SecondComponent() {
-  return (
-    <div className="SecondComponent">Second Component</div>
-  )
-}
-
-class ThirdComponent extends Component {
-  render() {
-    return (
-      <div className='ThirdComponent'>Third Component</div>
-    );
-  }
-}
-
-class FourthComponent extends Component {
-  render() {
-    return (
-      <div className='FourthComponent'>Fourth Component</div>
-    );
-  }
 }
 
 export default App;
