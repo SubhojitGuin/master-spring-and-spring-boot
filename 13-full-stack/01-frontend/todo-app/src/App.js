0f75e11eda628ec9195d55cd066419963975d3d2
Subhojit Guin <subhojitguin2004@gmail.com>
2025-12-21 21:29:43 +0530

    Add Counter component and update App to utilize it

--- 13-full-stack/01-frontend/todo-app/src/App.js
+++ 13-full-stack/01-frontend/todo-app/src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
-import LearningComponent from './components/learning-examples/LearningComponent';
+import Counter from './components/counter/Counter';
 
 function App() {
   return (
     <div className="App">
-      My Todo Application
-      <LearningComponent></LearningComponent>
+      <Counter />
     </div>
   );
 }
