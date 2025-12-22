cbc79a38b2030450b17d85a1a3f3e37f96783ba8
Subhojit Guin <subhojitguin2004@gmail.com>
2025-12-22 18:44:12 +0530

    Refactor Counter component to CounterButton with dynamic increment and decrement functionality and set the propType and defaultProp

--- 13-full-stack/01-frontend/todo-app/src/App.js
+++ 13-full-stack/01-frontend/todo-app/src/App.js
@@ -1,12 +1,37 @@
 import './App.css';
-import Counter from './components/counter/Counter';
+import CounterButton from './components/counter/Counter';
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      {/* <PlayWithProperties property1="value1" property2="value2"></PlayWithProperties> */}
+      <CounterButton by={1}/>
+      <CounterButton by={2}/>
+      <CounterButton by={5}/>
     </div>
   );
 }
+
+// {property1: 'value1', property2: 'value2'}
+// function PlayWithProperties(properties) {
+  
+//   console.log(properties);
+//   console.log(properties.property1);
+//   console.log(properties.property2);
+
+//   return (
+//     <div>Play With Properties</div>
+//   );
+// }
+
+// function PlayWithProperties({ property1, property2 }) {
+  
+//   console.log(property1);
+//   console.log(property2);
+
+//   return (
+//     <div>Play With Properties</div>
+//   );
+// }
 
 export default App;
--- 13-full-stack/01-frontend/todo-app/src/components/counter/Counter.jsx
+++ 13-full-stack/01-frontend/todo-app/src/components/counter/Counter.jsx
@@ -1,22 +1,40 @@
 import { useState } from 'react';
+import { PropTypes } from 'prop-types';
 import './Counter.css';
 
-export default function Counter() {
+export default function CounterButton({ by }) {
 
   const [count, setCount] = useState(0); // returns 2 things - Current State, function to update the state
 
   function incrementCounterFunction() {
-    setCount(count + 1);
+    setCount(count + by);
+  }
+
+  function decrementCounterFunction() {
+    setCount(count - by);
   }
 
   return (
     <div className="Counter">
-      <span className="count">{count}</span>
+      <span className="count">{ count }</span>
       <div>
         <button className="counterButton" 
             onClick={ incrementCounterFunction }
-        >+1</button>
+        >+{ by }</button>
+        <button className="counterButton"
+            onClick={ decrementCounterFunction }
+        >-{ by }</button>
       </div>
     </div>
   )
+}
+
+// Set the datatype of the properties
+CounterButton.propTypes = {
+  by: PropTypes.number
+}
+
+// Set default values of the properties if they are not specified while calling the element
+CounterButton.defaultProps = {
+  by: 1
 }
