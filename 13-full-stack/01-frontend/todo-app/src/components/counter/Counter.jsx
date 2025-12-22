d72012faca0fd63c4217327f61a5b7f80715097e
Subhojit Guin <subhojitguin2004@gmail.com>
2025-12-22 09:11:40 +0530

    Implement counter functionality with state

--- 13-full-stack/01-frontend/todo-app/src/components/counter/Counter.jsx
+++ 13-full-stack/01-frontend/todo-app/src/components/counter/Counter.jsx
@@ -1,14 +1,17 @@
+import { useState } from 'react';
 import './Counter.css';
 
 export default function Counter() {
 
+  const [count, setCount] = useState(0); // returns 2 things - Current State, function to update the state
+
   function incrementCounterFunction() {
-    console.log("increment");
+    setCount(count + 1);
   }
 
   return (
     <div className="Counter">
-      <span className="count">0</span>
+      <span className="count">{count}</span>
       <div>
         <button className="counterButton" 
             onClick={ incrementCounterFunction }
