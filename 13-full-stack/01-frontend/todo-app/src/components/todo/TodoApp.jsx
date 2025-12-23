cec34a71ea716126f8be3bcafb7ff88ce9a23057
Subhojit Guin <subhojitguin2004@gmail.com>
2025-12-23 19:26:00 +0530

    Create SuccessMessageComponent and ErrorMessageComponent to show success and failure of Authentication.

--- 13-full-stack/01-frontend/todo-app/src/components/todo/TodoApp.jsx
+++ 13-full-stack/01-frontend/todo-app/src/components/todo/TodoApp.jsx
@@ -15,6 +15,8 @@ function LoginComponent() {
 
   const [username, setUsername] = useState('Subho');
   const [password, setPassword] = useState('');
+  const [showSuccessMessage, setShowSuccessMessage]= useState(false);
+  const [showErrorMessage, setShowErrorMessage]= useState(false);
 
   function handleUsernameChange(event) {
     setUsername(event.target.value);
@@ -24,8 +26,34 @@ function LoginComponent() {
     setPassword(event.target.value);
   }
 
+  function handleSubmit() {
+    if (username === "Subho" && password === "dummy") {
+      setShowSuccessMessage(true);
+      setShowErrorMessage(false);
+    } else {
+      setShowSuccessMessage(false);
+      setShowErrorMessage(true);
+    }
+  }
+
+  function SuccessMessageComponent() {
+    if (showSuccessMessage)
+      return <div className="successMessage">Authenticated Sucessfully</div>;
+    else   
+      return null;
+  }
+
+  function ErrorMessageComponent() {
+    if (showErrorMessage)
+      return <div className="errorMessage">Authentication Failed!!! Please check your credentials</div>;
+    else   
+      return null;
+  }
+
   return (
     <div className="Login">
+      <SuccessMessageComponent/>
+      <ErrorMessageComponent/>
       <div className="LoginForm">
         <div>
           <label>User Name:</label>
@@ -36,7 +64,7 @@ function LoginComponent() {
           <input type="password" name="password" value={password} onChange={handlePasswordChange}/>
         </div>
         <div>
-          <button type="button" name="login">Login</button>
+          <button type="button" name="login" onClick={handleSubmit}>Login</button>
         </div>
       </div>
     </div>
