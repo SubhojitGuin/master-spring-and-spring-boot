778f82fe2cb3d5c899f561c59b240b8dc3029012
Subhojit Guin <subhojitguin2004@gmail.com>
2025-12-24 14:39:32 +0530

    Refactor core components for TodoApp including Header, Footer, Logout, Login, Welcome, ListTodos, and Error handling

---
+++ 13-full-stack/01-frontend/todo-app/src/components/todo/ListTodosComponent.jsx
@@ -0,0 +1,39 @@
+export function ListTodosComponent() {
+
+  const today = new Date();
+  const targetDate = new Date(today.getFullYear() + 12, today.getMonth(), today.getDay());
+
+  const todos = [
+    { id: 1, description: "Learn AWS", done: false, targetDate: targetDate },
+    { id: 2, description: "Learn Azure", done: false, targetDate: targetDate },
+    { id: 3, description: "Learn DevOps", done: false, targetDate: targetDate },
+    { id: 4, description: "Learn GCP", done: false, targetDate: targetDate }
+  ];
+  return (
+    <div className='container'>
+      <h1>Things you want to do!</h1>
+      <div>
+        <table className='table'>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Description</th>
+              <th>Is Done?</th>
+              <th>Target Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {todos.map(todo => (
+              <tr key={todo.id}>
+                <td>{todo.id}</td>
+                <td>{todo.description}</td>
+                <td>{todo.done.toString()}</td>
+                <td>{todo.targetDate.toDateString()}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
