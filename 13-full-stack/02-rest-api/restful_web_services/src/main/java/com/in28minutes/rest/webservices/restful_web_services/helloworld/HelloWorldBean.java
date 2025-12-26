0085df168c62c3fec807aeadbf2e2d4f2cc85dff
Subhojit Guin <subhojitguin2004@gmail.com>
2025-12-26 15:22:17 +0530

    Refactor HelloWorldBean constructor

--- 13-full-stack/02-rest-api/restful_web_services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldBean.java
+++ 13-full-stack/02-rest-api/restful_web_services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldBean.java
@@ -2,11 +2,11 @@ package com.in28minutes.rest.webservices.restful_web_services.helloworld;
 
 public class HelloWorldBean {
 
-    private String message;
-
     public HelloWorldBean(String message) {
         this.message = message;
     }
+
+    private String message;
 
     public String getMessage() {
         return message;
