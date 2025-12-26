0b059744d339f94df159850635eefb2f91fc120b
Subhojit Guin <subhojitguin2004@gmail.com>
2025-12-26 19:33:06 +0530

    Use axios to make GET call to backend/hello-world

--- 13-full-stack/01-frontend/todo-app/src/components/todo/WelcomeComponent.jsx
+++ 13-full-stack/01-frontend/todo-app/src/components/todo/WelcomeComponent.jsx
@@ -1,14 +1,34 @@
 import { useParams, Link } from "react-router-dom";
+import axios from "axios";
 
 export function WelcomeComponent() {
 
   const { username } = useParams();
 
+  function callHelloWorldRestApi() {
+    console.log('called');
+
+    axios.get('http://localhost:8080/hello-world') // axios returns a Promise
+        .then((response) => successfulResponse(response)) // sets the next action during successful response
+        .catch((error) => errorResponse(error)) // handles the error if the call is unsuccessful
+        .finally(() => console.log('cleanup')); // provides the final steps that are executed at any circumstances
+  }
+
+  function successfulResponse(response) {
+    console.log(response);
+  }
+
+  function errorResponse(error) {
+    console.log(error);
+  }
   return (
     <div className="WelcomeComponent">
       <h1>Welcome {username}</h1>
       <div>
         Manage Your todos - <Link to="/todos">Go here</Link>
+      </div>
+      <div>
+        <button className="btn btn-success m-5" onClick={callHelloWorldRestApi}>Call Hello World</button>
       </div>
     </div>
   );
