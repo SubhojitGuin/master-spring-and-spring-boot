97dffa93ce95870a66f07a45197d7ef09c1a6396
Subhojit Guin <subhojitguin2004@gmail.com>
2026-01-01 18:51:32 +0530

    Move executeBasicAuthenticationService to AuthenticationApiService

--- 13-full-stack/01-frontend/todo-app/src/components/todo/api/HelloWorldApiService.js
+++ 13-full-stack/01-frontend/todo-app/src/components/todo/api/HelloWorldApiService.js
@@ -11,9 +11,4 @@ import { apiClient } from "./ApiClient";
 export const retrieveHelloWorldPathVariable
   = (username) => apiClient.get(`/hello-world/path-variable/${username}`);
 
-export const executeBasicAuthenticationService
-  = (token) => apiClient.get(`/basicauth`, {
-    headers: {
-      Authorization: token
-    }
-  });
+
