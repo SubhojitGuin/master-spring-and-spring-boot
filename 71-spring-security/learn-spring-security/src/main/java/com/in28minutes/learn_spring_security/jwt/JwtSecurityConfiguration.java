92da9bf79a64d9b11ecf3e52bb600cba838827ed
Subhojit Guin <subhojitguin2004@gmail.com>
2026-01-04 18:42:35 +0530

    Add JWT security configuration and OAuth2 resource server support

--- 71-spring-security/learn-spring-security/src/main/java/com/in28minutes/learn_spring_security/jwt/JwtSecurityConfiguration.java
+++ 71-spring-security/learn-spring-security/src/main/java/com/in28minutes/learn_spring_security/jwt/JwtSecurityConfiguration.java
@@ -1,4 +1,10 @@
-package com.in28minutes.learn_spring_security.basic;
+package com.in28minutes.learn_spring_security.jwt;
+
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,16 +19,24 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPublicKey;
+import java.util.UUID;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
-//@Configuration
-public class BasicAuthSecurityConfiguration {
+@Configuration
+public class JwtSecurityConfiguration {
 
     enum Role {
         USER,
@@ -39,30 +53,14 @@ public class BasicAuthSecurityConfiguration {
                 session -> {
                     session.sessionCreationPolicy(SessionCreationPolicy.STATELESS); // Make the session stateless by not creating any session
                 });
-//        http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         http.csrf(AbstractHttpConfigurer::disable);
+        http.oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults())); // Enables OAuth2 Resource Server support
         http.headers(
                 headers -> headers.frameOptions(
                         HeadersConfigurer.FrameOptionsConfig::sameOrigin));  // To enable frames from the same origin
         return http.build();
     }
-
-    // This allows storing multiple users InMemory (non-persistent)
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        UserDetails user = User.withUsername("Subho")
-//                .password("{noop}dummy") // Since, the password is not encoded, we use {noop}
-//                .roles(Role.USER.name())
-//                .build();
-//
-//        UserDetails admin = User.withUsername("admin")
-//                .password("{noop}dummy")
-//                .roles(Role.ADMIN.name())
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(user, admin);
-//    }
 
     //  Provides a datasource to store the User details
     @Bean
@@ -99,5 +97,49 @@ public class BasicAuthSecurityConfiguration {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public KeyPair keyPair() { // 1: Create Key Pair
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            return keyPairGenerator.generateKeyPair();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Bean
+    public RSAKey rsaKey(KeyPair keyPair) { // 2: Provide the RSA key object
+        return new RSAKey
+                .Builder((RSAPublicKey) keyPair.getPublic())
+                .privateKey(keyPair.getPrivate())
+                .keyID(UUID.randomUUID().toString())
+                .build();
+    }
+
+    @Bean
+    public JWKSource<SecurityContext> jwkSource(RSAKey rsaKey) {
+        JWKSet jwkSet = new JWKSet(rsaKey); // 3a: Creates a new JWKSet
+//        return new JWKSource<>() {
+//            @Override
+//            public List<JWK> get(JWKSelector jwkSelector, SecurityContext context) {
+//                return jwkSelector.select(jwkSet);
+//            }
+//        };
+        return (jwkSelector, context) -> jwkSelector.select(jwkSet); // 3b: Creates a new JWKSource using the JWKSet
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(RSAKey rsaKey) throws JOSEException { // 4: Create the decoder
+        return NimbusJwtDecoder
+                .withPublicKey(rsaKey.toRSAPublicKey())
+                .build();
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder(JWKSource<SecurityContext> jwkSource) { // 5: Create the encoder
+        return new NimbusJwtEncoder(jwkSource);
     }
 }
