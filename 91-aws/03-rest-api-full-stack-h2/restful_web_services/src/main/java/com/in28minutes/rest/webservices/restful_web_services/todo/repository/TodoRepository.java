1a09e0d789ec6eb5f92903e3be140e9daf834978
Subhojit Guin <subhojitguin2004@gmail.com>
2026-01-11 21:10:56 +0530

    Add new project 03-rest-api-full-stack-h2 (AWS)

---
+++ 91-aws/03-rest-api-full-stack-h2/restful_web_services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/todo/repository/TodoRepository.java
@@ -0,0 +1,13 @@
+package com.in28minutes.rest.webservices.restful_web_services.todo.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.in28minutes.rest.webservices.restful_web_services.todo.Todo;
+
+public interface TodoRepository extends JpaRepository<Todo, Integer>{
+	
+	List<Todo> findByUsername(String username);
+
+}
